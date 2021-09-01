0f00732aa2f3c6c1998df57d3b60128842d2fa5c
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-01 12:36:52 +0300

    Add dependencies

--- config/server.js
+++ config/server.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const app = require("express")();
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const port = process.env.PORT || 3030;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("*", (req, res) => {
+  res.send("Welcome to Mamlaka Farm");
+});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
