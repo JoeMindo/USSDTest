fb4796051beb59a32e2b9ca94c3df2866864deee
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-13 16:03:10 +0300

    AB#1523 Add responses after each input

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ import logger from "morgan";
 import axios from "axios";
 import { use } from "chai";
 import { registerUser, clearData } from "./core/usermanagement";
-
-dotenv.config();
 const port = process.env.PORT || 3030;
 
 let app = express();
@@ -23,23 +21,27 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/ussd", (req, res) => {
+app.post("/ussd", (req, res) => {
   let { sessionId, serviceCode, phoneNumber, text } = req.body;
   let textValue = text.split("*").length;
 
   if (text == "") {
     message = `CON Welcome to Mamlaka Foods\n 1. Proceed`;
+    res.send(message)
   } else if (textValue === 1) {
     message = `CON
     Enter your name
     `;
     userDetails.name = text;
+    res.send(message)
   } else if (textValue === 2) {
     message = `CON What is your ID number`;
     userDetails.Id = text.split("*")[1];
+    res.send(message)
   } else if (textValue === 3) {
     message = `CON Which phone number would you like us to reach you at?`;
     userDetails.phone = text.split("*")[2];
+    res.send(message)
   } else if (textValue === 4) {
     message = `CON Enter your password`;
     userDetails.password = text.split("*")[3];
@@ -49,23 +51,26 @@ app.get("/ussd", (req, res) => {
     2. Buyer
     `;
     userDetails.role = text.split("*")[4];
+    res.send(message)
   } else if (textValue === 6) {
     message = `CON Complete registration
     1. Yes
     2. No`;
+    res.send(message)
     if (text.split("*")[5] === "Yes") {
       registerUser(userDetails, sessionId, phoneNumber);
       clearData();
       message = `END Thank you for registering`;
+      res.send(message)
     }
   } else {
     message = `END Thank you for trying out Mamlaka Foods`;
     clearData();
+    res.send(message)
   }
-  res.send(message,200)
 });
-const server = app.listen(port, () => {
+
+app.listen(port, () => {
   console.log(`Server running on port ${port}, `);
 });
 
-export { server, app };
