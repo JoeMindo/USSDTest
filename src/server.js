3559e11438a89caeceed95699ddb301edecb256c
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-14 09:44:06 +0300

    AB#1523 Update menu updating functionality

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import bodyParser from "body-parser";
 import logger from "morgan";
@@ -6,43 +5,41 @@ import { registerUser, clearData } from "./core/usermanagement.mjs";
 const port = process.env.PORT || 3030;
 
 const app = express();
-let message = "";
-let userDetails = {
-  name: "",
-  Id: "",
-  phone: "",
-  password: "",
-  role: "",
-};
-let textValue;
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/ussd", (req, res) => {
+  let message = "";
+  let userDetails = {
+    name: "",
+    Id: "",
+    phone: "",
+    password: "",
+    role: "",
+  };
+  let textValue;
   let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  
 
   if (text === "1") {
     message = `CON Welcome to Mamlaka Foods\n 1. Proceed`;
-    textValue = text.split("*").length
-    res.send(message)
-  }
-  
-  else if (textValue === 1) {
+    textValue = text.split("*").length;
+    res.send(message);
+  } else if (textValue === 1) {
     message = `CON
     Enter your name
     `;
     userDetails.name = text.split("*")[1];
-    res.send(message)
+    res.send(message);
   } else if (textValue === 3) {
     message = `CON What is your ID number`;
     userDetails.Id = text.split("*")[2];
-    res.send(message)
+    res.send(message);
   } else if (textValue === 4) {
     message = `CON Which phone number would you like us to reach you at?`;
     userDetails.phone = text.split("*")[3];
-    res.send(message)
+    res.send(message);
   } else if (textValue === 5) {
     message = `CON Enter your password`;
     userDetails.password = text.split("*")[4];
@@ -52,26 +49,25 @@ app.post("/ussd", (req, res) => {
     2. Buyer
     `;
     userDetails.role = text.split("*")[5];
-    res.send(message)
+    res.send(message);
   } else if (textValue === 7) {
     message = `CON Complete registration
     1. Yes
     2. No`;
-    res.send(message)
+    res.send(message);
     if (text.split("*")[6] === "Yes") {
       registerUser(userDetails, sessionId, phoneNumber);
       clearData();
       message = `END Thank you for registering`;
-      res.send(message)
+      res.send(message);
     }
   } else {
     message = `END Thank you for trying out Mamlaka Foods`;
     clearData();
-    res.send(message)
+    res.send(message);
   }
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}, `);
 });
-
