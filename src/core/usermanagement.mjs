989bf7ac10be7023432de144e2ebc9f7a1bc181c
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-14 10:58:01 +0300

    AB#1523 Update menu updating functionality

--- src/core/usermanagement.mjs
+++ src/core/usermanagement.mjs
@@ -10,7 +10,7 @@ let clearData = (userDetails) => {
 };
 let registerUser = (user) => {
   try {
-    let response = axios.post("https:/morning-woodland-97546/ussd", user);
+    let response = axios.post("https:/morning-woodland-97546/ussd/user", user);
     return response.data;
   } catch (error) {
     return error;
