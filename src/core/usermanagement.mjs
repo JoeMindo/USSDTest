272480fb09c3258b78ba2f16fa59d7975eabb8cd
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-15 10:47:59 +0300

    Add login screen

--- src/core/usermanagement.mjs
+++ src/core/usermanagement.mjs
@@ -4,6 +4,7 @@ import { response } from "express";
 //   'X-Requested-With': 'XMLHttpRequest',
 //   'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 // }
+
 let clearData = (details) => {
   details.name = "";
   details.Id = "";
@@ -13,21 +14,33 @@ let clearData = (details) => {
 
   return details;
 };
-let registerUser = (user) => {
+let registerUser = async (user) => {
   axios
-    .post("https://a326-197-211-5-78.ngrok.io/api/register", user)
+    .post("https://a326-197-211-5-78.ngrok.io/api/register", {
+      
+        first_name: user.first_name,
+        last_name: user.last_name,
+        phone_number: user.phone_number,
+        id_no: user.id_no,
+        email: user.email,
+        gender: user.gender,
+        password: user.password,
+        password_confirmation: user.password_confirmation
+      
+    })
     .then((response) => {
-      console.log(response.data)
+      console.log(response.data.message)
       return response.data;
     })
     .catch((error) => {
-      console.log(error.error);
+      console.log(error);
     });
 };
 let loginUser = (user) => {
   axios
     .post("https://a326-197-211-5-78.ngrok.io/api/login", user)
     .then((response) => {
+      console.log(response.data.message)
       return response.data;
     })
     .catch((error) => {
@@ -38,3 +51,14 @@ let loginUser = (user) => {
 let logoutUser = (id) => {};
 let updateUser = (id) => {};
 export { clearData, registerUser };
+// registerUser({
+//   first_name: "Joe",
+//   last_name: "Someone",
+//   id_no: "357414700",
+//   email: "joe@test.com",
+//   password: "password123",
+//   gender: "Male",
+//   password_confirmation: "password123"
+// }).then((response) => {
+//   console.log(response)
+// })
