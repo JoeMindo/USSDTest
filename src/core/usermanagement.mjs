59f0d19d9b3cb6aa53b8fc4b3b5a1c54c0023d57
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-21 10:23:20 +0300

    Update server link

--- src/core/usermanagement.mjs
+++ src/core/usermanagement.mjs
@@ -11,7 +11,7 @@ let clearData = (details) => {
   return details;
 };
 let registerUser = async (regdata) => {
-  var path = 'https://3a40-197-211-5-78.ngrok.io/api/register'
+  var path = 'https://987b-197-211-5-78.ngrok.io/api/register'
   var postdata = {
       "phone_no":regdata.phone_no,
     "first_name": regdata.first_name,
@@ -45,7 +45,7 @@ let registerUser = async (regdata) => {
 }
 
 let loginUser = async (loginData) => {
-  var path = 'https://3a40-197-211-5-78.ngrok.io/api/login'
+  var path = 'https://987b-197-211-5-78.ngrok.io/api/login'
   var postdata = {
     "phone_no": loginData.phone_no,
     "password": loginData.password,
