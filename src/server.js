ce612869f8ecb0bb908b93d72cd2964ed28b1177
JoeMindo <josephmindo@adalabsafrica.com>
2021-09-23 13:02:12 +0300

    Modify registration

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // const RedisStore = connectRedis(session);
 
-let client = redis.createClient();
+let client = redis.createClient('redis-19100.c251.east-us-mz.azure.cloud.redislabs.com:19100');
 client.on('connect', () =>{
   console.log('connected');
 });
